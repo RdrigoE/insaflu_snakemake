72ff5017fc54be90397918e90c6c742d923e4fd9
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2022-09-26 13:59:59 +0100

    create reference only if it does not exist

--- utils/create_snpeff_text.py
+++ utils/create_snpeff_text.py
@@ -1,12 +1,15 @@
+import subprocess
 import sys
 import re
 from Bio import SeqIO
 import os
 
 snpeff_path = sys.argv[1]
-ref_path = sys.argv[2]
+ref_path_gb = sys.argv[2]
 locus = sys.argv[3]
-reference_name = sys.argv[4]
+ref_path_fa = sys.argv[4]
+reference_name = sys.argv[5]
+output = sys.argv[6]
 
 if locus == 'Flu':
     text = [f"{reference_name}.chromosome : 1, 2, 3, 4, 5, 6, 7, 8\n",
@@ -20,7 +23,7 @@ if locus == 'Flu':
     f"{reference_name}.7.codonTable : Bacterial_and_Plant_Plastid\n",
     f"{reference_name}.8.codonTable : Bacterial_and_Plant_Plastid\n"]
 else:
-    version = SeqIO.parse(ref_path, "genbank")
+    version = SeqIO.parse(ref_path_gb, "genbank")
 
     for i in list(version.records):
         identification = i.annotations['accessions'][-1]
@@ -41,7 +44,23 @@ with open('config/snpeff.config' , 'r') as f:
     joined = ''.join(lines)
     find = joined.find(''.join(text))
 
+snpeff_path = sys.argv[1]
+ref_path_gb = sys.argv[2]
+locus = sys.argv[3]
+ref_path_fa = sys.argv[4]
+reference_name = sys.argv[5]
+output = sys.argv[6]
+
 if find == -1:
+    subprocess.run(f"mkdir  config/data/{reference_name} -p", shell=True)
+    subprocess.run( f"cat {ref_path_gb} > config/data/{reference_name}/genes.gbk", shell=True)
+    subprocess.run( f"cat {ref_path_fa} > config/data/{reference_name}/sequences.fa", shell=True)
     with open('config/snpeff.config', 'a') as snpeff:
         snpeff.writelines(text)
+    
+    subprocess.run( f"snpEff build -genbank {reference_name} -c config/snpeff.config > {output}", shell=True)
+else:
+    with open(output, 'w') as out:
+        out.write("Database Ready!")
+
 
