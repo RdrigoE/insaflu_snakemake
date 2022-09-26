ff3b916a64c200c8c70556f43d1044f8d4eaec54
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2022-09-26 08:02:25 +0100

    [fix] Create depth files for msa masker

--- utils/split_depth_file.py
+++ utils/split_depth_file.py
@@ -1,15 +1,15 @@
 import sys
-
+import re
 file_path = sys.argv[1]
-
 
 last_pos = len(file_path) - file_path[::-1].index("/")
 filename = file_path[last_pos:]
 new_file = []
 
+reference = x = re.findall("(?<=__)(.*?)(?=.depth)",filename)[0]
 with open(file_path, "r") as f:
     for line in f.readlines():
-        if line.split()[0] == filename[-7]:
+        if line.split()[0] == reference:
             new_file.append(line)
 with open(file_path, "w") as f:
     f.writelines(new_file)   
