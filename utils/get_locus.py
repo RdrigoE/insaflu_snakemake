eebbd9914cb63d3fc4323bd52e6c270335df0f2c
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2022-10-11 10:56:06 +0100

    [add] new script

--- utils/get_locus.py
+++ utils/get_locus.py
@@ -16,3 +16,18 @@ def get_locus(genbank_file):
     for record in SeqIO.parse(handle_gb, "genbank"):
         locus.append(record.name)
     return locus
+
+def get_id_version(genbank_file):
+    """
+    The get_locus function takes a genbank file and returns the locus number of that record.
+    If there is only one record in the genbank file, it will return the possible_name. If there are multiple records, 
+    it will return a list of all locus numbers.
+    
+    :param genbank_file: Open the genbank file, and then parse it using seqio
+    :param possible_name: Determine if the file is a single genbank file or not
+    :return: A list of locus numbers
+    :doc-author: Trelent
+    """
+    handle_gb = open(genbank_file)
+    for record in SeqIO.parse(handle_gb, "genbank"):
+         return record.id
